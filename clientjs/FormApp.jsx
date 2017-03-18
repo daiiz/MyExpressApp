a5d46fb28419065cbe5a73c52b18b16320eb499e
daiiz <da.iz.mg@gmail.com>
2017-03-19 00:01:04 +0900

    Reduxの基本形を写経

---
+++ clientjs/FormApp.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+// Container Components
+export default class FormApp extends React.Component {
+  render () {
+    return (
+      <div>
+        <FormInput handleClick={this.props.onClick} />
+        <FormDisplay data={this.props.value} />
+      </div>
+    );
+  }
+}
+
+FormApp.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  value  : React.PropTypes.string
+};
+
+
+// Presentational Components
+class FormInput extends React.Component {
+  send (e) {
+    e.preventDefault();
+    this.props.handleClick(this.myInput.value.trim());
+    this.myInput.value = '';
+    return;
+  }
+
+  render () {
+    return (
+      <form>
+        <input type="text" ref={(ref) => (this.myInput = ref)} defaultValue="" />
+        <button onClick={(event) => this.send(event)}>Send</button>
+      </form>
+    );
+  }
+}
+
+FormInput.propTypes = {
+  handleClick: React.PropTypes.func.isRequired
+};
+
+
+class FormDisplay extends React.Component {
+  render() {
+    return (
+      <div>{this.props.data}</div>
+    );
+  }
+}
+
+FormDisplay.propTypes = {
+  data: React.PropTypes.string
+};
--- clientjs/apps.jsx
+++ clientjs/apps.jsx
@@ -1,8 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+// https://gyazo.com/3d3a64aa24769f5d5d7fd268ea3dc7a7
+// https://github.com/maechabin/redux-sample
+// http://mae.chab.in/archives/2885
+
+/**
+ * View
+ */
+import FormApp from './FormApp.jsx';
 import Text from './Text.jsx';
 
+/**
+ * Actions
+ */
+const SEND = 'SEND';
 
-$(function () {
-  ReactDOM.render(<Text text="Hello World!"></Text>, $('#app')[0]);
-});
+let send = (value) => {
+  return {
+    type: SEND,
+    value: value
+  };
+};
+
+
+/**
+ * Reducers
+ */
+let formReducer = (state, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return Object.assign({}, state, {
+        value: action.value
+      });
+    default:
+      return state;
+  }
+};
+
+
+/**
+ * Store
+ */
+const initialState = {
+  value: null
+};
+const store = createStore(formReducer, initialState);
+
+
+/**
+ * Connect to Redux
+ */
+let mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    value: state.value
+  };
+};
+
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    onClick(value) {
+      dispatch(send(value));
+    }
+  };
+};
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormApp);
+
+
+/**
+ * Rendering
+ */
+ReactDOM.render(
+  <t>
+    <Text text="Hello React-Redux!!" />
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  </t>,
+  document.querySelector('#app')
+);
