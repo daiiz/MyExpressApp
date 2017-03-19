95dfa0c866196f044738c94f15c2fc1b8065f70a
daiiz <da.iz.mg@gmail.com>
2017-03-20 02:48:06 +0900

    readme

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 - React
 - GoogleCloudDatastore
 
-#### Dev
+### Devtool
 - Webpack
+
+### URL
+- `/`: 静的な描画の練習
+- `/app`: 簡単な入力フォームの練習
+  - https://github.com/maechabin/redux-sample を写経
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@
  * Routes list
  */
 var routes = {
-  demo: require('../controller/demo'),
-  app : require('../controller/formapp')
+  demo : require('../controller/demo'),
+  app  : require('../controller/formapp'),
+  album: require('../controller/album')
 };
 
 module.exports = routes;
